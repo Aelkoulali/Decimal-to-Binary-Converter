1c5ed19178cfa2387ee76d9225eb8de527596e39
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-07-27 16:57:11 +0200

    Variables declaration

---
+++ script.js
@@ -0,0 +1,5 @@
+// Access to the id from the html document
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
