6bb1542dc11d6b61a04f56670c905d07f3c7b9b4
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-07-31 23:01:52 +0200

    Add if statement to decimalToBinary function

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ const result = document.getElementById("result");
 // Function that do the decimal to binary conversion
 const decimalToBinary = (input) => {
     let binary = "";
+    if (input == 0) {
+      binary = "0";
+    };
+    
     while (input > 0) {
-        let binary = input % 2;
-        input = Math.floor(input / 2);
+      binary = (input % 2) + binary;
+      input = Math.floor(input / 2);
       
       };  
     result.innerText = binary;      
