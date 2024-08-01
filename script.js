8bc5621dee94a9c50d951c6eb5d2b5ac5b806bd5
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-01 11:40:10 +0200

    Add if ...else statement

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const result = document.getElementById("result");
 const decimalToBinary = (input) => {
   if ( input == 0){
     return "";
-  };
+  } else {
+    return decimalToBinary(Math.floor(input / 2));
+  }
     
 };
 
