0a6197a32a495a39d89324c2f968f3134d1c0dd5
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-02 17:37:47 +0200

    Add paragraph element

--- script.js
+++ script.js
@@ -37,6 +37,10 @@ const showAnimation = () => {
   result.innerText = "Call Stack Animation";
   animationData.forEach(obj => {
     setTimeout(() => {
+      animationContainer.innerHTML += 
+      `
+      <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">decimalToBinary(${obj.inputVal})</p>
+      `;
 
     }, obj.addElDelay);
 
